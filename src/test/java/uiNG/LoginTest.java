919c47726509e22c18a1825b48ebdf6354e3204c
Miika Immonen <immomipe@student.jyu.fi>
2022-11-04 11:41:57 +0200

    Basic annotations aftertest,beforetest,aftermethod,beforemethod

--- src/test/java/uiNG/LoginTest.java
+++ src/test/java/uiNG/LoginTest.java
@@ -1,5 +1,9 @@
 package uiNG;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
@@ -9,15 +13,47 @@ import org.testng.annotations.Test;
  *
  */
 public class LoginTest {
+	
+	/**
+	 * Runs before tests
+	 */
+	@BeforeTest
+	public void loginToApplication() {
+		System.out.println("Login to application");
+	}
+	
+	/**
+	 * Runs after tests
+	 */
+	@AfterTest
+	public void logOutFromApplication() {
+		System.out.println("Logout from application");
+	}
+	
+	/**
+	 * Runs before each test
+	 */
+	@BeforeMethod
+	public void establishConnection() {
+		System.out.println("Establishing connection");
+	}
+	
+	/**
+	 * Runs after each test
+	 */
+	@AfterMethod
+	public void disconnect() {
+		System.out.println("Disconnected");
+	}
 
-	@Test(priority=1, description="User can login")
-	public void loginTest() {
-		System.out.println("Login is succesful");
+	@Test(priority=1, description="A primary test")
+	public void Test1() {
+		System.out.println("First test");
 
 	}
 
-	@Test(priority=2, description="User can logout")
-	public void logoutTest() {
-		System.out.println("Logout is succesful");
+	@Test(priority=2, description="A secondary test")
+	public void Test2() {
+		System.out.println("Second test");
 	}
 }
